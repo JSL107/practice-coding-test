8bfa5d69ecd6e8b1305a922cf2aa058164e44750
JSL107 <juneseok0107@naver.com>
2024-12-15 17:10:38 +0900

    2024-12-15 일곱 난쟁이 문제 풀이 링크 : https://flax-moat-573.notion.site/15d69cbba39480a79938ef564b03c335?pvs=4

---
+++ coding-test/src/main/java/com/practice/codingtest/BOJ2309.java
@@ -0,0 +1,30 @@
+package com.practice.codingtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ2309 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] small = new int[9];
+        for (int i = 0; i < 9; i++) {
+            small[i] = Integer.parseInt(br.readLine());
+        }
+        int sum = Arrays.stream(small).sum() - 100;
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (small[i] + small[j] == sum) {
+                    small[i] = 0;
+                    small[j] = 0;
+                    Arrays.sort(small);
+                    for (int k = 2; k < 9; k++) {
+                        System.out.println(small[k]);
+                    }
+//                    return;
+                }
+            }
+        }
+    }
+}
