782c7f318818cb7a711dcf1ac8f109cfec88805d
JSL107 <juneseok0107@naver.com>
2024-12-18 00:00:28 +0900

    2024-12-17 소수 찾기 문제 풀이 링크 : https://flax-moat-573.notion.site/15f69cbba39480fabe9df97159aa50fb?pvs=4

---
+++ coding-test/src/main/java/com/practice/codingtest/소수찾기/BOJ1978.java
@@ -0,0 +1,29 @@
+package com.practice.codingtest.소수찾기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] numbers = br.readLine().split(" ");
+        int count;
+        int result = 0;
+        int number;
+        for (int i = 0; i < N; i++) {
+            number = Integer.parseInt(numbers[i]);
+            count = 1;
+            for (int j = 2; j <= number; j++) {
+                if (number % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
