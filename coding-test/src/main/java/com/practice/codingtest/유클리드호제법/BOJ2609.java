8545bd02264382a190116927e7a5a38bcbb87105
JSL107 <juneseok0107@naver.com>
2024-12-17 23:10:59 +0900

    2024-12-17 최대공약수와 최소공배수 문제 풀이 링크 : https://flax-moat-573.notion.site/15f69cbba394802ca81ff429ac7e8d6b?pvs=4

---
+++ coding-test/src/main/java/com/practice/codingtest/유클리드호제법/BOJ2609.java
@@ -0,0 +1,30 @@
+package com.practice.codingtest.유클리드호제법;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2609 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] numbers = br.readLine().split(" ");
+        int a = Integer.parseInt(numbers[0]);
+        int b = Integer.parseInt(numbers[1]);
+        System.out.println(GCD(a,b));
+        System.out.println(LCM(a,b));
+    }
+
+    public static int GCD(int a, int b) {
+        int temp;
+        while (b != 0) {
+            temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int LCM(int a, int b) {
+        return a * b / GCD(a, b);
+    }
+}
